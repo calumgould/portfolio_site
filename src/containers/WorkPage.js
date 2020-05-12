27df447f7c2af8575e0e3b5cdfeda219ea5b58a9
calumgould <calumgould26@gmail.com>
2020-05-12 19:25:32 +0100

    added background images & styling to work page

--- src/containers/WorkPage.js
+++ src/containers/WorkPage.js
@@ -1,14 +1,32 @@
 import React from 'react';
+import designImage from 'assets/images/design_background.jpg';
+import programmingImage from 'assets/images/programming_background.jpg';
 
 const WorkPage = ({history}) => {
+
+    const designBackground = {
+        backgroundImage: `url(${designImage})`
+    }
+
+    const programmingBackground = {
+        backgroundImage: `url(${programmingImage})`
+    }
+
     return ( 
+        
         <div>
             <h1>Work</h1>
-            <div onClick={() => history.push('/work/design')}>
-                <h2>Design</h2>
-            </div>
-            <div onClick={() => history.push('/work/programming')}>
-                <h2>Programming</h2>
+            <div className='work-page'>
+                <div className='half-page' 
+                    onClick={() => history.push('/work/design')}
+                    style={designBackground}>
+                    <h2>Design</h2>
+                </div>
+                <div className='half-page' 
+                    onClick={() => history.push('/work/programming')}
+                    style={programmingBackground}>
+                    <h2>Programming</h2>
+                </div>
             </div>
         </div>
      );
